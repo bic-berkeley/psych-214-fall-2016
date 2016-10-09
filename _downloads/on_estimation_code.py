dc26b6e8084cc03c69cc05ec81c4d610acb4c96e
Matthew Brett <matthew.brett@gmail.com>
2016-10-08 22:54:57 -0800

    Update documentation

--- _downloads/on_estimation_code.py
+++ _downloads/on_estimation_code.py
@@ -29,11 +29,55 @@ import matplotlib.pyplot as plt
 
 #- Make second column orthononal to first. Confirm orthogonality
 
+
+""" What is the relationship between the values on the diagonal of
+X_o.T.dot(X_o) and the lengths of the vectors in the first and second
+columns of X_o?
+
+"""
+
+
+
+""" What is the relationship between the values on the diagonal of the
+*inverse* of X_o.T.dot(X_o) and the lengths of the vectors in the first
+and second columns of X_o?
+
+"""
+
+
 #- Make mean-centered version of psychopathy vector
 
 #- Calculate fit of X_o to y_o
 
+
+""" Explain the new value of the first element of the parameter estimate
+vector.
+
+"""
+
+
 #- Correlation coefficient of y_c and the second column of X_o
 
+
+""" What is the relationship between the correlation coefficient "r_xy"
+and the second element in the parameter vector "B_o[1]"?
+
+"""
+
+
 #- Fit X_o to psychopathy data
+
+
+
+""" Explain the relationship between the mean of the psychopathy values
+and the first element of the parameter estimate vector.
+
+"""
+
+
+
+""" Why is the second value in B_o the same when estimating against "y_c"
+and "psychopathy"?
+"""
+
 
---
+++ on_estimation_solution-20.py
@@ -0,0 +1,3 @@
+fitted_X_o = X_o.dot(B_o)
+np.allclose(fitted_X_o, fitted)
+# True
